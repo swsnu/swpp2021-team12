a81f8bb66b959cac5b526cc27426a801e08145ea
rkdgnlgus2 <rkdgnlgus2@snu.ac.kr>
2021-10-30 02:20:25 +0900

    FIX minor code smells

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -3,6 +3,8 @@ from .models import User
 import json
 
 class UserTestCase(TestCase):
+
+    
     
     def test_superuser(self):
         User.objects.create_superuser(email="rr@rr.rr",password="ff")
@@ -28,29 +30,33 @@ class UserTestCase(TestCase):
 
     def test_sign_fail(self):
         client = Client()
+        signin = '/api/user/sign_in/'
+        signup = '/api/user/sign_up/'
+        signout = '/api/user/sign_out/'
+        jsontype = "application/json"
 
-        response = client.post('/api/user/sign_in/',
-        json.dumps({'email':'tt@tt.tt','password':'tt'}),content_type="application/json")     
+        response = client.post(signin,
+        json.dumps({'email':'tt@tt.tt','password':'tt'}),content_type=jsontype )     
         self.assertEqual(response.status_code,401)
-        response = client.post('/api/user/sign_in/',
-        json.dumps({'email':'tt@tt.tt'}),content_type="application/json") 
+        response = client.post(signin,
+        json.dumps({'email':'tt@tt.tt'}),content_type=jsontype ) 
         self.assertEqual(response.status_code,400)
-        response = client.get('/api/user/sign_in/')
+        response = client.get(signin)
         self.assertEqual(response.status_code,405)
 
-        response = client.get('/api/user/sign_out/')
+        response = client.get(signout)
         self.assertEqual(response.status_code,401)
-        response = client.post('/api/user/sign_out/')
+        response = client.post(signout)
         self.assertEqual(response.status_code,405)
 
 
-        response = client.post('/api/user/sign_up/',
-        json.dumps({'email':'tt@tt.tt','password':'tt'}),content_type="application/json")
+        response = client.post(signup,
+        json.dumps({'email':'tt@tt.tt','password':'tt'}),content_type=jsontype )
         self.assertEqual(response.status_code,400)
-        response = client.post('/api/user/sign_up/',
-        json.dumps({'email':None,'password':None,'name':None}),content_type="application/json")
+        response = client.post(signup,
+        json.dumps({'email':None,'password':None,'name':None}),content_type=jsontype )
         self.assertEqual(response.status_code,409)
-        response = client.get('/api/user/sign_up/')
+        response = client.get(signup)
         self.assertEqual(response.status_code,405)
 
         
@@ -60,11 +66,12 @@ class UserTestCase(TestCase):
 
     def test_token(self):
         client = Client()
-        response = client.get('/api/user/token/')
+        token = '/api/user/token/'
+        response = client.get(token )
 
         self.assertEqual(response.status_code,204)
 
-        response = client.post('/api/user/token/')
+        response = client.post(token )
 
         self.assertEqual(response.status_code,405)
 
