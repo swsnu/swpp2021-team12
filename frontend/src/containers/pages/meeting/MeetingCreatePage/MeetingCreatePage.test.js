c15d3ca9d1fc89d69a8858a985b008583d1ae9de
KIMJunOh <kjo1797@gmail.com>
2021-11-13 04:58:25 +0900

    FIX MeetingCreatePage.test

--- frontend/src/containers/pages/meeting/MeetingCreatePage/MeetingCreatePage.test.js
+++ frontend/src/containers/pages/meeting/MeetingCreatePage/MeetingCreatePage.test.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-undef */
 
 import React from "react";
+import axios from 'axios';
 import { mount } from "enzyme";
 import configureMockStore from 'redux-mock-store';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import MeetingCreatePage from "./MeetingCreatePage";
+
+jest.mock('axios');
 
 describe('<MeetingCreatePage />', () => {
     jest.spyOn(window, 'alert').mockImplementation(() => {});
@@ -30,22 +33,21 @@ describe('<MeetingCreatePage />', () => {
         expect(wrapper.length).toBe(1);
     })
     it('sholud handle onClickConfirmHandler well', () => {
-        const spyClickConfirmHandler = jest.fn();
-        component = mount(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <MeetingCreatePage onClickConfirmHandler={spyClickConfirmHandler}/>
-                </BrowserRouter>
-            </Provider>
-        );
+        axios.post.mockImplementation((url, data) =>
+            Promise.resolve(data)
+            )
         const titleInput = component.find('#meeting-title-input').find('input');
         titleInput.simulate('change', { target: { value: 'title' } });
         const contentInput = component.find('#meeting-content-input').find('textarea');
         contentInput.simulate('change', { target: { value: 'content'} });
         const maxMembersInput = component.find('#meeting-max-members-input').find('input');
         maxMembersInput.simulate('change', { target: { value: 9} });
+        /*  changed submit -> onClick
         const submit = component.find('#meeting-create-form').find('Form');
         submit.simulate('submit');
-        expect(spyClickConfirmHandler).toHaveBeenCalledTimes(0);
+        */
+        const confirmButton = component.find('#confirm-button').find('button');
+        confirmButton.simulate('click');
+        expect(axios.post).toHaveBeenCalledTimes(1);
     })
 })
