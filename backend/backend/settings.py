17e125bdbc6db079c58b9fbbb54e056983f2f713
KylusheL <ez2dj1010@naver.com>
2021-11-13 11:50:31 +0900

    meeting backend coverage 100

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'user.apps.UserConfig',
     'meeting.apps.MeetingConfig',
-    'comment.apps.CommentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
