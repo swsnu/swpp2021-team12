235436b35132485cf7dfe696a4dc8e93a9047ad5
KylusheL <ez2dj1010@naver.com>
2021-11-20 03:44:46 +0900

    club-detail first commit

--- frontend/src/containers/pages/meeting/MeetingDetailPage/MeetingDetailPage.js
+++ frontend/src/containers/pages/meeting/MeetingDetailPage/MeetingDetailPage.js
@@ -4,29 +4,9 @@ import axios from 'axios';
 import MeetingDetail from '../../../../components/meeting/MeetingDetail';
 import CommentList from '../../../../components/comment/CommentList/CommentList';
 import PageTemplate from '../../../common/PageTemplate';
-// import {
-//   createComment,
-//   editComment,
-//   deleteComment,
-//   getMeetingComments,
-// } from '../../../../store/actions/comments';
 
 // TODO: Photo, Access, Tag, Location, Time
 function MeetingDetailPage(props) {
-  // const tempComments = [
-  //   {
-  //     id: 11,
-  //     articleId: 11,
-  //     authorId: 1,
-  //     content: 'Hello this is test comment',
-  //   },
-  //   {
-  //     id: 12,
-  //     articleId: 11,
-  //     authorId: 2,
-  //     content: 'second comment for test',
-  //   },
-  // ];
   const [meetingDetail, setMeetingDetail] = useState(null);
   const [comments, setComments] = useState(null);
   const [refresh, setRefresh] = useState(false);
@@ -35,21 +15,17 @@ function MeetingDetailPage(props) {
   }));
   const { history } = props;
   const { id } = props.match.params;
-  // let dataReducer = { members: [] };
 
   useEffect(() => {
     axios
       .get(`/api/meeting/${id}/`)
       .then((res) => {
         setMeetingDetail(res.data);
-        // dataReducer = { ...dataReducer, meetingData: res.data };
       })
       .then(() => {
         axios.get(`/api/comment/meeting/${id}/`).then((res) => {
           setComments(res.data);
         });
-        console.log('===fetched comments===');
-        console.log(comments);
       })
       .catch(() => {
         window.alert('Error occured while fetching meeting info');
