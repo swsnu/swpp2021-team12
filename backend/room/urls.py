9d91bcb98fa330affd450d98c3e7a4842d0fce51
KIMJunOh <kjo1797@gmail.com>
2021-11-22 00:36:47 +0900

    FIX room/urls.py

--- backend/room/urls.py
+++ backend/room/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from room import views
 
 urlpatterns = [
-    
+    path('', views.register_room, name='register_room'),
+    path('/<int:room_id>/', views.room, name='room'),
+    path('/room/host/', views.host_room, name='host_room'),
 ]
