77d8b944de95d6b3bb378f85104230a63d31f231
rkdgnlgus2 <rkdgnlgus2@snu.ac.kr>
2021-12-10 14:53:38 +0900

    Applying .gitignore

--- frontend/src/lib/api/club.js
+++
@@ -1,2 +0,0 @@
-export const club = '/api/club/';
-export const clubJoined = '/api/club/joined/';
--- frontend/src/lib/api/meetings.js
+++
@@ -1,4 +0,0 @@
-export const meetings = '/api/meeting/';
-export const meetingDetail = '/api/meeting/:id/';
-export const meetingToggle = '/api/meeting/:id/toggle/';
-export const meetingAuthor = '/api/meeting/author/:author_id/';
